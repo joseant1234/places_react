d689231c603926918cbc25db3bc479b0e9a538c8
Jose Antonio <j-osea-nt@hotmail.com>
2017-11-16 22:27:48 -0500

    add card in app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
+import { Card, CardText } from 'material-ui/Card';
+
+// https://material.io/guidelines/style/color.html
+import {indigo400, redA400, lightBlueA400, amberA400 } from 'material-ui/styles/colors';
 
 import Title from './components/Title';
 
@@ -39,23 +43,56 @@ class App extends Component {
               <RaisedButton label="Crear cuenta gratuita" secondary={true} />
               <img className="Header-ilustration" src={process.env.PUBLIC_URL + '/images/top-background.png'} />
             </div>
-            <div>
-              <ul>
-                <li>
+          </div>
+        </div>
+
+        <div style={{'backgroundColor': indigo400, 'padding': '50px'}}>
+          <ul>
+
+            <Card class="Header-Benefit">
+              <CardText>
+                <div class="row">
+                  <div className="Header-Benefit-image style={{'backgroundColor': redA400}}">
+                    <img src={process.env.PUBLIC_URL + '/images/heart.png'} />
+                  </div>
+                </div>
+                <div className="Header-Benefit-content">
                   <h3>Calificaciones con emociones</h3>
                   <p>Califica tus lugares con experiencias, no con números</p>
-                </li>
-                <li>
+                </div>
+              </CardText>
+            </Card>
+
+            <Card class="Header-Benefit">
+              <CardText>
+                 <div class="row">
+                  <div className="Header-Benefit-image style={{'backgroundColor': lightBlueA400}">
+                    <img src={process.env.PUBLIC_URL + '/images/no-internet.png'} />
+                  </div>
+                </div>
+                <div className="Header-Benefit-content">
                   <h3>¿Sin Internet? Sin problemas</h3>
                   <p>Places funcionan sin internet o en conexiones lentas</p>
-                </li>
-                <li>
+                </div>
+              </CardText>
+            </Card>
+
+
+            <Card class="Header-Benefit">
+              <CardText>
+                 <div class="row">
+                  <div className="Header-Benefit-image style={{'backgroundColor': amberA400}}">
+                    <img src={process.env.PUBLIC_URL + '/images/star.png'} />
+                  </div>
+                </div>
+                <div className="Header-Benefit-content">
                   <h3>Tus lugares favoritos</h3>
                   <p>Define una lista de sitios favoritos</p>
-                </li>
-              </ul>
-            </div>
-          </div>
+                </div>
+              </CardText>
+            </Card>
+              
+          </ul>
         </div>
       </MuiThemeProvider>
     );
